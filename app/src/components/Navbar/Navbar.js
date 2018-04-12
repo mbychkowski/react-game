4c9b2542313bf73d6f4800a7c825e0d2e289a432
michael bychkowski <mbychkowski@gmail.com>
2018-04-11 21:30:28 -0400

    navbar and jumbotron added

--- app/src/components/Navbar/Navbar.js
+++ app/src/components/Navbar/Navbar.js
@@ -1,4 +1,16 @@
 import React from 'react';
-import '.Navbar.css';
+import './Navbar.css';
+
+const Navbar = props => {
+
+  return(
+  <nav className="navbar fixed-top navbar-light bg-light">
+    <a className="navbar-brand" href="#">
+      <img src={props.logo} className="App-logo" alt="React" />
+
+    </a>
+  </nav>)
+
+}
 
 export default Navbar;
